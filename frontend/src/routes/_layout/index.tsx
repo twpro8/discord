a788fe7a191f16167edda32d6ca8495d2225f7f1
John Davis <twpro8@gmail.com>
2026-06-18 17:43:20 +0300

    chore(ui): install and configure sonner for notifications

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -1,11 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router"
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export const Route = createFileRoute("/_layout/")({
-    component: () => (
-        <div className="flex h-screen w-screen items-center justify-center flex-col gap-2">
-            <p>Nothing here...</p>
-            <Button>Click me!</Button>
-        </div>
-    ),
+    component: RootLayout,
 })
+
+function RootLayout() {
+    return (
+        <div className="flex h-screen w-screen items-center justify-center flex-col gap-2">
+            <p>Don't click the button...</p>
+            <Button
+                onClick={() => {
+                    toast.error("Happy? Nothing here...");
+                }}
+            >
+                Show Toast
+            </Button>
+        </div>
+    )
+}
