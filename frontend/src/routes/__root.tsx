a4471d5d0f09e4141c4d3aef818b17ae37db2e21
John Davis <twpro8@gmail.com>
2026-06-18 16:51:15 +0300

    chore(frontend): add tanstack react query devtools

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Route = createRootRoute({
     component: () => (
         <>
             <HeadContent />
             <Outlet />
+            <ReactQueryDevtools initialIsOpen={false} />
             <TanStackRouterDevtools position="bottom-right" />
         </>
     ),
