e905e1a1634795715c023145a015a029bbb1e88f
AlexgDevs <asmetanin014@gmail.com>
2026-06-24 21:08:05 +0500

    chore: add exception handler

--- backend/src/core/errors/__init__.py
+++ backend/src/core/errors/__init__.py
@@ -1,7 +1,9 @@
 from .base import AppException, ObjectNotFoundError, ObjectAlreadyExistsError
+from .base_handler import app_exception_handler
 
 __all__ = [
     "AppException",
     "ObjectNotFoundError",
     "ObjectAlreadyExistsError",
+    "app_exception_handler",
 ]
