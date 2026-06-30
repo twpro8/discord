b9bf58f7ddd1a2b1426369049856738164503fd8
John Davis <twpro8@gmail.com>
2026-06-30 03:51:14 +0300

    feat(user): implement update user service #37

--- backend/src/user/repository.py
+++ backend/src/user/repository.py
@@ -1,3 +1,7 @@
+from typing import Any
+from uuid import UUID
+
+from asyncpg.exceptions import UniqueViolationError
 from pydantic import BaseModel
 from sqlalchemy.exc import IntegrityError
 
@@ -17,4 +21,25 @@ class UserRepository(BaseRepository[UserOrm, User]):
             user = await super().create(data)
         except IntegrityError:
             raise UserAlreadyExistsError
+        return user
+
+    async def update(
+        self,
+        id_: UUID,
+        data: BaseModel,
+        exclude_unset: bool = False,
+        **filter_by: Any,
+    ) -> User:
+        try:
+            user = await super().update(id_, data, exclude_unset)
+        except IntegrityError as e:
+            cause = getattr(e.orig, "__cause__", None)
+            constraint = getattr(cause, "constraint_name", None)
+            if isinstance(cause, UniqueViolationError):
+                match constraint:
+                    case "users_email_key":
+                        raise UserAlreadyExistsError("Email already exists") from e
+                    case "users_username_key":
+                        raise UserAlreadyExistsError("Username already exists") from e
+            raise
         return user
