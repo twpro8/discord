3ec72831f6a6f09c0471efeadab3270443f0fd12
John Davis <twpro8@gmail.com>
2026-06-30 03:43:17 +0300

    feat(auth): add field restrictions to the register form

--- backend/src/auth/schemas.py
+++ backend/src/auth/schemas.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import EmailStr
+from pydantic import EmailStr, Field
 
 from src.core.schemas import BaseSchema
 
 
 class RegisterForm(BaseSchema):
-    name: str
-    username: str
-    email: EmailStr
-    password: str
+    name: str = Field(min_length=3, max_length=64)
+    username: str = Field(min_length=3, max_length=32)
+    email: EmailStr = Field(min_length=3, max_length=32)
+    password: str = Field(min_length=3, max_length=128)
 
 
 class TokenPair(BaseSchema):
