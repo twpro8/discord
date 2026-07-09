f5578a05dfb566d85f2d80139c74833efa33c435
AlexgDevs <asmetanin014@gmail.com>
2026-07-09 17:00:42 +0500

    feat(invite): add uow #111

---
+++ backend/src/server/invite/unit_of_work.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.unit_of_work.base_unit_of_work import BaseUnitOfWork
+from src.server.invite.repository import ServerInviteRepository
+from src.server.repository import ServerRepository
+
+
+class ServerInviteUnitOfWork(BaseUnitOfWork):
+    invites: ServerInviteRepository
+    servers: ServerRepository
+
+    def __init__(
+        self,
+        session: AsyncSession,
+        server_invite_repository: ServerInviteRepository,
+        server_repository: ServerRepository,
+    ) -> None:
+        super().__init__(session)
+        self.invites = server_invite_repository
+        self.servers = server_repository
+
+    def _uow_marker(self) -> None: ...
