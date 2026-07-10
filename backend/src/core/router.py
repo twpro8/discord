745f68187cc970a7ab049383f8806d03be07f79a
AlexgDevs <asmetanin014@gmail.com>
2026-07-10 19:34:01 +0500

    feat(invite): implement POST, GET, DELETE invites #121 #122 #123

--- backend/src/core/router.py
+++ backend/src/core/router.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter
 from src.user.router import router as user_router
 from src.auth.router import router as auth_router
 from src.chat.router import router as chat_router
-from src.server import router as server_router
+from src.server.router import router as server_router
+from src.server.invite.router import router as server_invite_router
 
 api_router = APIRouter()
 api_router.include_router(auth_router)
 api_router.include_router(user_router)
 api_router.include_router(chat_router)
 api_router.include_router(server_router)
+api_router.include_router(server_invite_router)
