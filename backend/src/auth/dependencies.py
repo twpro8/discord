11aded7f8a6dd11df2c5519655119d45b6e43ad6
John Davis <twpro8@gmail.com>
2026-07-25 17:39:20 +0300

    feat(auth): migrate to cookie auth flow #0 - Update dependencies - Update auth router - Update conftest.py

--- backend/src/auth/dependencies.py
+++ backend/src/auth/dependencies.py
@@ -2,12 +2,16 @@ from collections.abc import AsyncGenerator
 from typing import Annotated
 
 from fastapi import Depends
+from fastapi.security import APIKeyCookie
 
+from src.auth.exceptions import AuthenticationError
 from src.auth.repository import RefreshTokenRepository
 from src.auth.service import AuthService
 from src.auth.unit_of_work import AuthUnitOfWork
 from src.core.dependencies import SessionDep
 from src.user.dependencies import UserRepositoryDep
+
+refresh_cookie_scheme = APIKeyCookie(name="refresh_token", auto_error=False)
 
 
 def get_refresh_token_repository(session: SessionDep) -> RefreshTokenRepository:
@@ -34,8 +38,16 @@ def get_auth_service(
     return AuthService(session, auth_unit_of_work)
 
 
+def get_refresh_token_cookie(token: OptionalRefreshTokenDep) -> str:
+    if token is None:
+        raise AuthenticationError
+    return token
+
+
 RefreshTokenRepositoryDep = Annotated[
     RefreshTokenRepository, Depends(get_refresh_token_repository)
 ]
 AuthUnitOfWorkDep = Annotated[AuthUnitOfWork, Depends(get_auth_unit_of_work)]
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
+OptionalRefreshTokenDep = Annotated[str | None, Depends(refresh_cookie_scheme)]
+RefreshTokenDep = Annotated[str, Depends(refresh_cookie_scheme)]
