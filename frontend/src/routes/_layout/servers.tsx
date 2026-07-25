9e0bff0a9fff83ae7670a47624d6f0575d5b6ec4
Rustem Amirkhanuly <musulmazhamza@gmail.com>
2026-07-25 13:29:34 +0500

    refactor(frontend): deleted old version of frontend

--- frontend/src/routes/_layout/servers.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import ServerPage from "@/pages/ServerPage.tsx";
-
-export const Route = createFileRoute('/_layout/servers')({
-  component: () => <ServerPage />,
-})
