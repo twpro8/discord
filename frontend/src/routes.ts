2218ca93813a66866b8725976607ea1720ad48e8
Rustem Amirkhanuly <musulmazhamza@gmail.com>
2026-07-25 13:29:34 +0500

    refactor(frontend): deleted old version of frontend

--- frontend/src/routes.ts
+++
@@ -1,7 +0,0 @@
-export const ROUTES = {
-    DASHBOARD: "/",
-    LOGIN: "/login",
-    SIGNUP: "/signup",
-    SERVERS: "/servers",
-    CHATS: "/chats",
-} as const
--- frontend/src/routes/_layout/chats.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import ChatPage from "@/pages/ChatPage.tsx";
-
-export const Route = createFileRoute('/_layout/chats')({
-    component: () => <ChatPage />,
-})
