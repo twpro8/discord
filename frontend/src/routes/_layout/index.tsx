185fd8ad836aae4db4803a978fd758e89d631535
Rustem Amirkhanuly <musulmazhamza@gmail.com>
2026-07-25 13:29:34 +0500

    refactor(frontend): deleted old version of frontend

--- frontend/src/routes/_layout/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router"
-import { RootPage } from "@/pages/RootPage.tsx";
-
-export const Route = createFileRoute("/_layout/")({
-    component: RootPage,
-})
--- frontend/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { getErrorMessage, getInitials, colorFromId } from "./common"
-export { formatRelativeTime } from "./time"
-export { toChatEntry } from "./mappers"
