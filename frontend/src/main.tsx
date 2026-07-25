ef35e886124beedf09853962d19bfd8640dcee7e
Rustem Amirkhanuly <musulmazhamza@gmail.com>
2026-07-25 18:07:20 +0500

    refactor(frontend): added toaster for showing messages and zustand for storage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import { routeTree } from './routeTree.gen'
+import { SonnerProvider } from './shared/ui/sonner-provider'
 
 const queryClient = new QueryClient()
 const router = createRouter({ routeTree })
@@ -17,6 +18,7 @@ declare module '@tanstack/react-router' {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <SonnerProvider />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
