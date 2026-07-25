dbb4d7d0c63a572ca7ee1400edd73d31993f6b08
Rustem Amirkhanuly <musulmazhamza@gmail.com>
2026-07-25 13:29:34 +0500

    refactor(frontend): deleted old version of frontend

--- frontend/src/routes/signup.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { SignupPage } from "@/pages/SignupPage";
-
-export const Route = createFileRoute("/signup")({
-    component: () => <SignupPage />,
-})
