8011003ed33dbf4282075ce3dfba148950b508da
John Davis <twpro8@gmail.com>
2026-07-30 01:06:35 +0300

    refactor(servers): remove unused exceptions

--- backend/src/modules/servers/domain/exceptions.py
+++ backend/src/modules/servers/domain/exceptions.py
@@ -24,21 +24,6 @@ class CannotTransferToSelfError(ServerError):
     detail = "You cannot transfer to self"
 
 
-class OwnerCannotLeaveServerError(ServerError):
-    status_code = status.HTTP_403_FORBIDDEN
-    detail = "Owner cannot leave to the server"
-
-
-class CannotKickSelfError(ServerError):
-    status_code = status.HTTP_403_FORBIDDEN
-    detail = "User cannot kick self"
-
-
-class OnlyOwnerCanKickError(ServerError):
-    status_code = status.HTTP_403_FORBIDDEN
-    detail = "Only owner can kick"
-
-
 class YouAreNotOwnerError(ServerError):
     status_code = status.HTTP_403_FORBIDDEN
     detail = "You are not owner"
